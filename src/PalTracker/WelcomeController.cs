3627b7fdfda4a6aaf5bc33fc4727dab059ec6620
unknown <IMGADMIN@D-113109697.wipro.com>
2019-04-08 16:53:37 +0530

    2 simple .net core web api app

--- src/PalTracker/WelcomeController.cs
+++ src/PalTracker/WelcomeController.cs
@@ -5,7 +5,43 @@ namespace PalTracker
     [Route("/")]
     public class WelcomeController : ControllerBase
     {
+       
+        private readonly WelcomeMessage _message;
+
         [HttpGet]
-        public string SayHello() => "hello";
+        public string SayHello() => _message.Message;
+
+        public WelcomeController(WelcomeMessage  message)
+        {
+            _message = message;
+        }
+    }
+
+
+
+     public class WelcomeMessage
+    {
+        public string Message { get; }
+
+        public WelcomeMessage(string message)
+        {
+            Message = message;
+        }
+    }
+
+      public class CloudFoundryInfo
+    {
+        public string Port { get; }
+        public string MemoryLimit { get; }
+        public string CfInstanceIndex { get; }
+        public string CfInstanceAddr { get; }
+
+        public CloudFoundryInfo(string port, string memoryLimit, string cfInstanceIndex, string cfInstanceAddr)
+        {
+            Port = port;
+            MemoryLimit = memoryLimit;
+            CfInstanceIndex = cfInstanceIndex;
+            CfInstanceAddr = cfInstanceAddr;
+        }
     }
 }
